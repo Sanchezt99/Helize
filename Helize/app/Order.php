7a61e032f10f318325b56a7f39ee86adb4a4f3f0
nroldanr <nroldanr@eafit.edu.co>
2020-09-14 15:47:49 -0500

    Orders and Items models, controllers and migrations

---
+++ Helize/app/Order.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Item;
+use App\User;
+
+class Order extends Model
+{
+    //attributes id, total, created_at, updated_at
+    protected $fillable = ['total'];
+
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id)
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getTotal()
+    {
+        return $this->attributes['total'];
+    }
+
+    public function setTotal($total)
+    {
+        $this->attributes['total'] = $total;
+    }
+
+    public function items(){
+        return $this->hasMany(Item::class);
+    }
+    
+    public function user(){
+        return $this->hasOne(User::class);
+    }
+}
